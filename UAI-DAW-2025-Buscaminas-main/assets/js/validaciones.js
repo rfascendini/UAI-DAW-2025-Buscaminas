32da541334aa3bc402b0d89671ac17239749bf68
Agustin Fretes <31964459+lunaticoz123@users.noreply.github.com>
2025-07-26 11:19:07 -0300

    Arreglando bug del contador , agregando sonidos y validaciones
    Hay que agregar los modales de victoria , derrota y validaciones  porque temporalmente estan echos con alert

---
+++ UAI-DAW-2025-Buscaminas-main/assets/js/validaciones.js
@@ -0,0 +1,52 @@
+const nombre =document.getElementById("nombre-contacto")
+const email =document.getElementById("email-contacto")
+const mensaje =document.getElementById("mensaje-contacto")
+const form =document.getElementById("form-contacto")
+const submit=document.getElementById("submit")
+
+
+
+
+function validateInputs()
+{
+const emailregex =/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
+const name=/^[a-zA-Z\-]+$/;
+
+
+if(name.test(nombre.value  ||  nombre.value=="")  ){
+alert("Complete el Nombre  ")
+nombre.style.border="1px solid red";
+return false
+
+}
+if(!emailregex.test(email.value)  || email.value=="")
+{
+alert("Ingresar el formato requerido  ej: email@ejemplo.com")
+email.style.border= "1px solid red";
+return false
+
+}
+if(mensaje.value===""){
+alert("Campo mensaje vacio")
+mensaje.style.border="1px solid red";
+return false
+
+}
+
+alert(" !Enviado, Muchas Gracias  ")
+localStorage.setItem("nombre",JSON.stringify(nombre));
+localStorage.setItem("email",JSON.stringify(email));
+localStorage.setItem("mensaje",JSON.stringify(mensaje));
+return true
+}
+
+
+document.addEventListener("click", (e)=>{
+if(e.target===submit){
+    e.preventDefault();
+    validateInputs();
+}
+
+})
+
+//Local Storage 
